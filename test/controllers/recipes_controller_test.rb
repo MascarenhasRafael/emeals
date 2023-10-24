580ea27810a44283bb9489d935e16996c6e61dc8
Rafael Mascarenhas <rafael.amorim@petanjo.com>
2023-10-24 11:20:00 -0300

    test: Add controller and model tests for Recipe

---
+++ test/controllers/recipes_controller_test.rb
@@ -0,0 +1,155 @@
+class RecipesControllerTest < ActionController::TestCase
+  before do
+    @recipe = recipes(:one)
+  end
+
+  describe 'GET #index' do
+    it 'responds successfully' do
+      get :index, format: :json
+      assert_response :success
+    end
+  end
+
+  describe 'POST #create' do
+    it 'creates a recipe successfully' do
+      assert_difference('Recipe.count') do
+        post :create, params: {
+          recipe: {
+            name: @recipe.name,
+            image_path: @recipe.image_path,
+            cook_time_in_seconds: @recipe.cook_time_in_seconds,
+            prep_time_in_seconds: @recipe.prep_time_in_seconds
+          }
+        }, format: :json
+      end
+
+      assert_response :created
+    end
+
+    it 'does not create a recipe without name' do
+      post :create, params: {
+        recipe: {
+          cook_time_in_seconds: @recipe.cook_time_in_seconds,
+          prep_time_in_seconds: @recipe.prep_time_in_seconds
+        }
+      }, format: :json
+
+      assert_response :unprocessable_entity
+      parsed_body = JSON.parse(response.body)
+      assert_not_nil parsed_body['name']
+      assert_includes parsed_body['name'], "can't be blank"
+    end
+
+    it 'does not create a recipe without cook_time_in_seconds' do
+      post :create, params: {
+        recipe: {
+          name: @recipe.name,
+          prep_time_in_seconds: @recipe.prep_time_in_seconds
+        }
+      }, format: :json
+
+      assert_response :unprocessable_entity
+      parsed_body = JSON.parse(response.body)
+      assert_not_nil parsed_body['cook_time_in_seconds']
+      assert_includes parsed_body['cook_time_in_seconds'], "can't be blank"
+    end
+
+    it 'does not create a recipe without prep_time_in_seconds' do
+      post :create, params: {
+        recipe: {
+          name: @recipe.name,
+          cook_time_in_seconds: @recipe.cook_time_in_seconds
+        }
+      }, format: :json
+
+      assert_response :unprocessable_entity
+      parsed_body = JSON.parse(response.body)
+      assert_not_nil parsed_body['prep_time_in_seconds']
+      assert_includes parsed_body['prep_time_in_seconds'], "can't be blank"
+    end
+  end
+
+  describe 'GET #show' do
+    it 'responds successfully' do
+      get :show, params: { id: @recipe }, format: :json
+      assert_response :success
+    end
+  end
+
+  describe 'PATCH #update' do
+    it 'updates a recipe successfully' do
+      patch :update, params: {
+        id: @recipe,
+        recipe: {
+          name: @recipe.name,
+          image_path: @recipe.image_path,
+          cook_time_in_seconds: @recipe.cook_time_in_seconds,
+          prep_time_in_seconds: @recipe.prep_time_in_seconds
+        }
+      }, format: :json
+
+      assert_response :success
+    end
+
+    it 'does not update a recipe without name' do
+      patch :update, params: {
+        id: @recipe,
+        recipe: {
+          name: nil,
+          image_path: @recipe.image_path,
+          cook_time_in_seconds: @recipe.cook_time_in_seconds,
+          prep_time_in_seconds: @recipe.prep_time_in_seconds
+        }
+      }, format: :json
+
+      assert_response :unprocessable_entity
+      parsed_body = JSON.parse(response.body)
+      assert_not_nil parsed_body['name']
+      assert_includes parsed_body['name'], "can't be blank"
+    end
+
+    it 'does not update a recipe without cook_time_in_seconds' do
+      patch :update, params: {
+        id: @recipe,
+        recipe: {
+          name: @recipe.name,
+          image_path: @recipe.image_path,
+          cook_time_in_seconds: nil,
+          prep_time_in_seconds: @recipe.prep_time_in_seconds
+        }
+      }, format: :json
+
+      assert_response :unprocessable_entity
+      parsed_body = JSON.parse(response.body)
+      assert_not_nil parsed_body['cook_time_in_seconds']
+      assert_includes parsed_body['cook_time_in_seconds'], "can't be blank"
+    end
+
+    it 'does not update a recipe without prep_time_in_seconds' do
+      patch :update, params: {
+        id: @recipe,
+        recipe: {
+          name: @recipe.name,
+          image_path: @recipe.image_path,
+          cook_time_in_seconds: @recipe.cook_time_in_seconds,
+          prep_time_in_seconds: nil
+        }
+      }, format: :json
+
+      assert_response :unprocessable_entity
+      parsed_body = JSON.parse(response.body)
+      assert_not_nil parsed_body['prep_time_in_seconds']
+      assert_includes parsed_body['prep_time_in_seconds'], "can't be blank"
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'destroys a recipe successfully' do
+      assert_difference('Recipe.count', -1) do
+        delete :destroy, params: { id: @recipe }, format: :json
+      end
+
+      assert_response :no_content
+    end
+  end
+end
---
+++ test/models/recipe_test.rb
@@ -0,0 +1,53 @@
+class RecipeTest < ActiveSupport::TestCase
+  def setup
+    @recipe = Recipe.new(
+      name: 'Spaghetti Bolognese',
+      cook_time_in_seconds: 1800,
+      prep_time_in_seconds: 900
+    )
+  end
+
+  def teardown
+    @recipe.destroy
+  end
+
+  test 'should be valid with valid attributes' do
+    assert @recipe.valid?
+    assert @recipe.save
+  end
+
+  test 'should require name' do
+    @recipe.name = nil
+    assert_not @recipe.valid?
+    assert_includes @recipe.errors[:name], "can't be blank"
+    assert_not @recipe.save
+  end
+
+  test 'should require cook_time_in_seconds to be present' do
+    @recipe.cook_time_in_seconds = nil
+    assert_not @recipe.valid?
+    assert_includes @recipe.errors[:cook_time_in_seconds], "can't be blank"
+    assert_not @recipe.save
+  end
+
+  test 'cook_time_in_seconds should be greater than or equal to 0' do
+    @recipe.cook_time_in_seconds = -10
+    assert_not @recipe.valid?
+    assert_includes @recipe.errors[:cook_time_in_seconds], 'must be greater than or equal to 0'
+    assert_not @recipe.save
+  end
+
+  test 'should require prep_time_in_seconds to be present' do
+    @recipe.prep_time_in_seconds = nil
+    assert_not @recipe.valid?
+    assert_includes @recipe.errors[:prep_time_in_seconds], "can't be blank"
+    assert_not @recipe.save
+  end
+
+  test 'prep_time_in_seconds should be greater than or equal to 0' do
+    @recipe.prep_time_in_seconds = -5
+    assert_not @recipe.valid?
+    assert_includes @recipe.errors[:prep_time_in_seconds], 'must be greater than or equal to 0'
+    assert_not @recipe.save
+  end
+end
